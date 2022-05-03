7d40765c4f8ded46e75a6d547467a3f6029e31c5
BreadButterCat <40633228+tulza@users.noreply.github.com>
2022-05-03 23:35:40 +1200

    Add project files.

---
+++ Just testing class/PersonCls.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Just_testing_class
+{
+    internal class SurveyedCitizen
+    {
+        //Citizen Info
+        //Member Variable <!>
+        string firstName;
+        string surname;
+        string townID;
+        int birthYear;
+        string surveyID;
+
+        //Database Info
+        int totalSurvey = 0; // To be implemented 
+
+
+        //The constructor <!>
+        public SurveyedCitizen(string ArgsFirstName, string ArgsSurname, string ArgTown, int ArgsBirthYear)
+        {
+            firstName = ArgsFirstName;
+            surname = ArgsSurname;
+            townID = ArgTown;
+            birthYear = ArgsBirthYear;
+
+            totalSurvey++;
+            //ID generation
+            surveyID = DateTime.Now.ToString(String.Format("{0} - {1:D2} ", DateTime.Now.ToString("yy/mm/dd"), SurveyNum(totalSurvey)));
+        }
+
+        //Citizen Info Reading
+        //Member Variable Property <!>
+        public string FirstName { 
+            get { return firstName; }}
+        public string Surname { 
+            get { return surname; }}
+        public int BirthYear { 
+            get { return birthYear; }}
+        public string TownID {
+            get { return townID; }}
+        public string SurveyID{
+            get { return surveyID; }}
+
+        //Member Method <!>
+        private static int SurveyNum(int TotalSurvey){ 
+            return TotalSurvey;
+        }
+    }
+}
---
+++ Just testing class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Just_testing_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SurveyedCitizen> SurveyData = new List<SurveyedCitizen>();
+
+            string[] TownID = new string[]
+            {
+                "DUNE","AUCK","WELT",
+                "CHSC","HAMT","INVC",
+            };
+            string[] TownName = new string[]
+            {
+                "Dunedin","Auckland","Wellinton",
+                "Christchurch","Hamilton","Invercargill",
+            };
+
+            SurveyData.Add(new SurveyedCitizen("hawkins", "kapo", TownID[1], 2004));
+            int a = 0;
+            Console.WriteLine($" [{SurveyData[a].TownID} {SurveyData[a].SurveyID}] {SurveyData[a].FirstName} {SurveyData[a].Surname} {SurveyData[a].BirthYear} ");
+        }
+    }
+}
